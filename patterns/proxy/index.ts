3be1a22db3c57a605cd0a5da5dc1cb423745c371
DANILO SAMWAYS <danilo.samways@madatech.com.br>
2024-11-22 16:48:31 -0300

    feat: add poxy pattern description

--- patterns/proxy/index.ts
+++ patterns/proxy/index.ts
@@ -1,3 +1,9 @@
+/** Proxy Pattern
+ *  Um proxy recebe solicitações, realiza alguma tarefa, e passa a solicitação
+ *  para um objeto de serviço. O proxy pode ser usado para adicionar segurança,
+ *  controle de acesso, log, etc.
+ */
+
 import { Service } from "./Service";
 import { ServiceProxy } from "./ServiceProxy";
 
